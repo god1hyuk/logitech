9fb5cf9406a1056d57f0a953651c266d65cfccfe
wonhyukshin <wonhyuk6340@gmail.com>
2021-05-10 11:59:14 +0900

    v2.6
    20210510

--- js/logitech.js
+++ js/logitech.js
@@ -1,30 +1,10 @@
-// $logo = $('#hero-logo-container');
-// $logoMobile = $('.mobile-logo');
-// $logo.html('<div id="hero-logo-ani" class="hero-logo-ani animating stoppedAnimation"></div>');
-// $logoMobile.html('<img class="mobile-dark-logo" src="/content/dam/logitech/common/en/logi-logo-dark.png" alt="logo"><img class="mobile-light-logo" src="/content/dam/logitech/common/en/logi-logo-white.png" alt="logo">');
-// $logiAni = $("#hero-logo-ani");
-
-// $logo.css('visibility', 'visible');
-
-// $logo.hover(function() {
-//         $logiAni
-//         .removeClass("stoppedAnimation")
-//         .addClass("hoverPlay");
-//     },
-//     function() {
-//         $logiAni
-//             .removeClass("hoverPlay")
-//             .addClass("stoppedAnimation");
-// });
-
-
 // aos
 AOS.init();
 
 
 $(document).ready(function(){
     $(".hamburger").click(function(){
-      $(this).toggleClass("is-active");
+      $(this).stop().toggleClass("is-active");
     });
   });
 
